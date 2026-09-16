21a780087d0bf27bbed8ca0713fa83a16d572ac1
titoamortica <baileychanningg@gmail.com>
2026-09-16 20:38:49 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -4,7 +4,9 @@
   Only use the public "anon" key here.
   NEVER put a service_role key in this file.
 */
-window.SUPABASE_URL = "https://supabase.com/dashboard/project/zfdomtkcthpbzdigvlaa/settings/general";
+
+window.SUPABASE_URL = "https://zfdomtkcthpbzdigvlaa.supabase.co";
+
 window.SUPABASE_ANON_KEY = "sb_publishable_8QlUqzMWI1xF8ycpmatgSw_4k2Yxtxz";
 
 /* The email you create for Tito's owner account in Supabase Auth. */
